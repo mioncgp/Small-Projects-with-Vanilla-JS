78508137db18d7d86a9fcbdf8d8c1c9afc37d67b
Michael <mioncgp@gmail.com>
2020-07-05 11:42:40 +0400

    done

--- 13_Text_to_speech/app.js
+++ 13_Text_to_speech/app.js
@@ -5,6 +5,7 @@ const readBtn = document.getElementById("read");
 const toggleBtn = document.getElementById("toggle");
 const closeBtn = document.getElementById("close");
 
+// data to upload images and messages
 const data = [
   {
     image: "./img/drink.jpg",
@@ -56,17 +57,92 @@ const data = [
   },
 ];
 
+// iterate through the objects and on each iteration call func
 data.forEach(createBox);
 
+// Create speech boxes
 function createBox(item) {
+  // create element
   const box = document.createElement("div");
-
+  // destructure the values
   const { image, text } = item;
 
   box.classList.add("box");
-  box.innerHTML = `
-  <img src="${image}" alt="${text}"/>
-  <p class="info">${text}</p>`;
 
+  box.innerHTML = `
+    <img src="${image}" alt="${text}" />
+    <p class="info">${text}</p>
+  `;
+  // when clicked say the text
+  box.addEventListener("click", () => {
+    setTextMessage(text);
+    speakText();
+
+    box.classList.add("active");
+    setTimeout(() => {
+      box.classList.remove("active");
+    }, 1000);
+  });
+  //   add boxes to the DOM
   main.appendChild(box);
 }
+
+// init the speech synthesis
+const message = new SpeechSynthesisUtterance();
+
+// show all the possible voices to select from
+function getVoices() {
+  // create an array from the API
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach((voice) => {
+    const option = document.createElement("option");
+
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+
+    voicesSelect.appendChild(option);
+  });
+}
+
+// pass the text to the initalized text class
+function setTextMessage(text) {
+  message.text = text;
+}
+
+// speak the text
+function speakText() {
+  speechSynthesis.speak(message);
+}
+
+// iterate through the voices and seleced one
+function setVoice(e) {
+  message.voice = voices.find((voice) => voice.name === e.target.value);
+}
+
+// read the text in the textarea
+function readText() {
+  setTextMessage(textarea.value);
+  speakText();
+}
+
+// if the voices is chaned get them again
+speechSynthesis.addEventListener("voiceschanged", getVoices);
+
+// show show or hide the text box
+toggleBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.toggle("show")
+);
+
+// remove the tex box
+closeBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.remove("show")
+);
+
+// when clicked read the text
+readBtn.addEventListener("click", readText);
+
+// when cliked set the selected voice to speak
+voicesSelect.addEventListener("change", setVoice);
+
+getVoices();
